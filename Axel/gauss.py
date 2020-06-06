57fec9d1828dab3389cea884ae2793443a1fe6dd
AntiDesert5 <axelvalenzuela7@gmail.com>
2020-06-06 18:29:47 -0500

    Gauss en python funciona con hilos
    borre metodo de jacobi en python ese no se pidio

--- Axel/gauss.py
+++ Axel/gauss.py
@@ -4,17 +4,45 @@ Created on Thu Jun  4 19:13:01 2020
 
 @author: Valenzuela
 """
-
 import numpy as np
+from threading import Thread
 
-A=np.array([[10,-1,2,0],
-            [-1,11,-1,3],
-            [2,-1,10,-1],
-            [0,3,-1,8]])
+def hilo(itera,m,n,r,matrix):
+    k=0
+    while k<itera:
+        suma=0
+        k=k+1
+        for r in range(0,m):
+            suma=0
+            for c in range(0,n):
+                if (c != r):
+                    suma=suma+matrix[r,c]*x[c]               
+            x[r]=(vector[r]-suma)/matrix[r,r]
+        del error[:]
+    for i in range(0,m):
+        print("x["+str(i)+"]: "+str(x[i]))
 
-b=np.array([6,25,-11,15])
+m=int(input('Valor de m:'))#renglones
+n=int(input('Valor de n:'))#columnas
+matrix = np.zeros((m,n))#matriz coeficientes
+x=np.zeros((m))#solucion
 
-x=np.linalg.solve(A,b)
+vector= np.zeros((n))
+comp=np.zeros((m))
+error=[]
 
-print(x)
+print ('Método de Gauss-Seidel')
+print ('Introduce la matriz de coeficientes y el vector solución')
+for r in range(0,m):
+    for c in range(0,n):
+        matrix[(r),(c)]=float(input("Elemento a["+str(r+1)+str(c+1)+"] "))
+    vector[(r)]=float(input('b['+str(r+1)+']: '))
+print ("Método de Gauss-Seidel")
 
+tol=float(input("cual es la tolerancia deseada?: "))
+itera=int(input("Cual es el numero maximo de iteraciones?: "))
+
+t = Thread(target=hilo, args=(itera,m,n,r,matrix))
+t.start()
+t.join()         
+#print(matrizr)
